00d110909d91049a36447f8aae5bd65076fa5b7c
Michael Mandiberg <firstname@lastname.com>
2014-06-03 16:34:04 -0400

    Touching the Archive Page
    Rebuilt the archive page with divs and added text descriptions.

--- lib/archive-social_hashtag.php
+++ lib/archive-social_hashtag.php
@@ -1,13 +1,33 @@
+<!--this file is The Loop for the archive page.-->
 <?php get_header(); ?>
 <section id="social_hashtags">
+
+
 	<?php if(have_posts()): ?>
-		<ul>
+
+		<div class="the-social-posts">
 		<?php while(have_posts()): the_post(); ?>
-			<li><a href="<?php the_permalink(); ?>"><?php the_content(); ?></a></li>
+			<div class="a-social-post">
+				<a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
+				<span class="social-description"><?php the_title(); ?></span>
+			</div>
 		<?php endwhile; ?>
+		</div>
+
+<!--		<ul>
+
+		<?php while(have_posts()): the_post(); ?>
+			<li>
+				<a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
+				<span class="social-description"><?php the_title(); ?></span>
+			</li>
+		<?php endwhile; ?>
+		
 		</ul>
+-->
 	<?php else: ?>
 		<h4>No Content was found</h4>
 	<?php endif; ?>
+		<p class="clear"> &nbsp; </p>
 </section>
 <?php get_footer(); ?>
