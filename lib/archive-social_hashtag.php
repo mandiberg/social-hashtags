915ac2b013fee16b84aa6b65197c6cfb5a9df070
Michael Mandiberg <firstname@lastname.com>
2014-06-03 19:28:56 -0400

    CSS tweaks
    EOD CSS tweaks. Commented out shortcodes.php

--- lib/archive-social_hashtag.php
+++ lib/archive-social_hashtag.php
@@ -1,4 +1,5 @@
 <!--this file is The Loop for the archive page.-->
+
 <?php get_header(); ?>
 <section id="social_hashtags">
 
@@ -9,25 +10,16 @@
 		<?php while(have_posts()): the_post(); ?>
 			<div class="a-social-post">
 				<a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
-				<span class="social-description"><?php the_title(); ?></span>
+				<p><?php the_title(); ?></p>
 			</div>
 		<?php endwhile; ?>
 		</div>
-
-<!--		<ul>
-
-		<?php while(have_posts()): the_post(); ?>
-			<li>
-				<a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
-				<span class="social-description"><?php the_title(); ?></span>
-			</li>
-		<?php endwhile; ?>
 		
-		</ul>
--->
+
 	<?php else: ?>
 		<h4>No Content was found</h4>
 	<?php endif; ?>
 		<p class="clear"> &nbsp; </p>
+		 <?php posts_nav_link( $sep, $prelabel, $nextlabel ); ?> 
 </section>
 <?php get_footer(); ?>
